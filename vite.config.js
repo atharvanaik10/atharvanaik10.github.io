f4bad1bed0d32d96a9e8cdddce4556aa7b617812
Atharva Naik <atharvanaik10@gmail.com>
2026-09-09 21:43:11 -0400

    Add separate staging deployment path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [svelte()],
   server: {
     host: '0.0.0.0'
