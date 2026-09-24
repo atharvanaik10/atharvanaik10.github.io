1f8b563288b923ac2c4050fa4ffb22a674969d90
Atharva Naik <atharvanaik10@gmail.com>
2026-09-24 15:45:30 -0400

    Tighten sections and modularize editorial cards

--- src/components/ResearchItem.svelte
+++ src/components/ResearchItem.svelte
@@ -1,34 +1,13 @@
 <script>
-  import { Button, Card, Icon } from 'm3-svelte'
-  import iconOpenInNew from '@ktibow/iconset-material-symbols/open-in-new'
-  import ItemSubheading from './ItemSubheading.svelte'
+  import EditorialItem from './EditorialItem.svelte'
   export let item
 </script>
 
-<article class="editorial-card" data-card>
-  <Card variant="outlined">
-    <div class="item-heading">
-      <h3 class="type-title">{item.name}</h3>
-      <p class="item-period type-meta">{item.period}</p>
-    </div>
-    <ItemSubheading left={item.role} rightHtml={item.locationHtml} />
-    <div class="prose-lite">{@html item.html}</div>
-    {#if item.tech && item.tech.length}
-      <div class="chip-row">
-        {#each item.tech as t}
-          <span class="skill-chip">{t}</span>
-        {/each}
-      </div>
-    {/if}
-    {#if item.links && item.links.length}
-      <div class="link-row">
-        {#each item.links as link}
-          <Button variant="tonal" href={link.url} target="_blank" rel="noreferrer" iconType="left">
-            <Icon icon={iconOpenInNew} size={18} />
-            {link.label}
-          </Button>
-        {/each}
-      </div>
-    {/if}
-  </Card>
-</article>
+<EditorialItem
+  title={item.name}
+  period={item.period}
+  role={item.role}
+  locationHtml={item.locationHtml}
+  html={item.html}
+  tech={item.tech}
+  links={item.links} />
